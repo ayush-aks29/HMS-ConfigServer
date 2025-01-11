53aae654203258d327ab62f190a8e4ccaec33398
Ayush Kumar Srivastava <ayushksrivastava29@gmail.com>
2025-01-11 22:06:11 +0530

    Completed Implementation of API Gateway

--- src/main/java/com/apiGateway/ApiGatewayApplication.java
+++ src/main/java/com/apiGateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.apiGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
